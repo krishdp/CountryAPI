ab9c3d0eb0b444f8dbf4c416bfbb8f117ad36aa4
Krishnandu Ranjan Roy <roykrish9899@gmail.com>
2022-03-28 16:25:00 +0530

    Adding API CRUD features.

---
+++ countries/serializers.py
@@ -0,0 +1,10 @@
+from turtle import mode
+from rest_framework import serializers
+from countries.models import Countries
+
+
+class CountriesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Countries
+        fields = ('id', 'name', 'capital')
